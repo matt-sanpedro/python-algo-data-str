8ff2b75a6410b57220b40eafd009338b4693868a
matt-sanpedro <m.sanpedro@tcs.com>
2023-06-22 15:47:33 -0700

    Added variable for keeping track of iterations

--- section-01-algorithm-big-o/intro.py
+++ section-01-algorithm-big-o/intro.py
@@ -1,17 +1,19 @@
 import time
 
+number_iterations = 1000000
+
 def sum1(n):
     # iteratively adds the numbers
     final_sum = 0
     for x in range(n+1):
-        print('x is: {}'.format(x))
+        # print('x is: {}'.format(x))
         final_sum += x
 
     print('Final sum is: {}'.format(final_sum))
     return final_sum
 
 start_time = time.time()
-sum1(5)
+sum1(number_iterations)
 # 0.001997709274291992 seconds
 print("--- %s seconds ---" % (time.time() - start_time))
 
@@ -21,6 +23,6 @@ def sum2(n):
     return final_sum
 
 start_time = time.time()
-sum2(5)
+sum2(number_iterations)
 # 0.0 seconds
 print("--- %s seconds ---" % (time.time() - start_time))
