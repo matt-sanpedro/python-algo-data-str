a478b0d086894ea9c8557281e0e2616e54737c51
matt-sanpedro <m.sanpedro@tcs.com>
2023-07-25 18:57:44 -0700

    Added additional test code

--- section-02-array-sequences/interview-problems/scratch.py
+++ section-02-array-sequences/interview-problems/scratch.py
@@ -2,3 +2,13 @@ unique_list = [(1,3),(2,2)]
 
 if (1,3) in unique_list:
     print('ok')
+
+my_string = ''
+my_string += '{}{}'.format('A',3)
+print(my_string)
+my_string += '{}{}'.format('C',6)
+print(my_string)
+
+test_string = 'AAAAABBBBCCCC'
+for letter in test_string[1:]:
+    print(letter)
