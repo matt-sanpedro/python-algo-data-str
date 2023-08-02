6574b5f21ccd0ce5af29b1aee0c5866492d6d9c1
mobius <mattsanpedro7@gmail.com>
2023-08-01 21:21:14 -0700

    Added implementation of queue using two stacks

--- section-03-stacks-queues-deques/interview-problems/p-05-queue-with-two-stacks.py
+++ section-03-stacks-queues-deques/interview-problems/p-05-queue-with-two-stacks.py
@@ -4,22 +4,51 @@ Implement a Queue - Using Two Stacks
 Given the Stack class below, implement a Queue class using **two** stacks! 
 Note, this is a "classic" interview problem. Use a Python list data structure as your Stack.
 
-DEFINITIONS
+DEFINITIONS:
 Stack: addition of new items and removal of existing items always takes place at the same end
 Queue: addition of new items happens at one end (rear/back -> enqueue), and the removal of existing items occurs at the other end (front -> dequeue)
+
+IMPLEMENTATION:
+Key insight is a stack reverses order, a queue does not.
+A sequence of elements pushed on a stack comes back in reversed order when popped.
+Thus, two stacks chained together will return elements in the same order.
 '''
 class Queue2Stacks(object):
 
     def __init__(self):
 
         # two stacks
-        self.stack1 = []
-        self.stack2 = []
+        self.instack = []
+        self.outstack = []
 
     def enqueue(self,element):
-        pass
+        print('Instack before enqueue: {}'.format(self.instack))
+        self.instack.append(element)
+        print('Instack after enqueue:  {}'.format(self.instack))
 
     def dequeue(self):
-        pass
+        print('Outstack before dequeue: {}'.format(self.outstack))
+        if not self.outstack:
+            while self.instack:
+                self.outstack.append(self.instack.pop())
 
-    
+        print('Outstack after dequeue:  {}'.format(self.outstack))
+        print('Instack after dequeue:   {}'.format(self.instack))
+        return self.outstack.pop()
+
+# TEST CLASS
+q = Queue2Stacks()
+for i in range(5):
+    q.enqueue(i)
+
+q.dequeue()
+q.enqueue(100)
+q.dequeue()
+q.dequeue()
+q.dequeue()
+q.dequeue()
+q.dequeue()
+
+# check stacks
+print('Final instack:  {}'.format(q.instack))
+print('Final outstack: {}'.format(q.outstack))
