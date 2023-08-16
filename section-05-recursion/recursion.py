09e132e572bbd5816566cd1fa619e4028cc85df8
matt-sanpedro <m.sanpedro@tcs.com>
2023-08-16 13:00:29 -0700

    Added comments

--- section-05-recursion/recursion.py
+++ section-05-recursion/recursion.py
@@ -1,9 +1,11 @@
 def fact(n):
+    print('n is: {}'.format(n))
 
     # base case must be defined or function will never end
     if n == 0:
         return 1
     else:
+        print('Computing: {}'.format(n * fact(n - 1)))
         return n * fact(n - 1)
     
 print(fact(5))
